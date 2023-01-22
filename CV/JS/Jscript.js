2daa3479f7bd2f71d892ae591d852299eec3a689
diegodelatorrre <123281260+diegodelatorrre@users.noreply.github.com>
2023-01-21 23:26:43 -0300

    Add files via upload

---
+++ CV/JS/Jscript.js
@@ -0,0 +1,11 @@
+
+const element = document.getElementById("fbtn");
+element.addEventListener("click", cabecera);
+
+function cabecera() {
+    console.log('Funciona el JS');
+    document.getElementById("h2mas").style.display="inline";
+ };
+
+
+ 
